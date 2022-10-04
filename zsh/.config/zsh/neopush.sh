c6df4f406052953b3e2d0aba47f1f4b741b1ecfc
MythicalNewt <mythicalnewt@github.com>
2022-10-04 17:19:30 +0530

    Backup script.

---
+++ zsh/.config/zsh/neopush.sh
@@ -0,0 +1,16 @@
+# This script automatically backs up zshrc and neovim configs
+
+#!/bin/zsh
+
+cd ~/git/MythicalNewt/Mythical-Dots
+
+git pull
+
+cp -r ~/.config/nvim/lua ~/git/MythicalNewt/Mythical-Dots/.config/nvim
+cp ~/.config/nvim/init.lua ~/git/MythicalNewt/Mythical-Dots/.config/nvim
+cp ~/.config/zsh/.zshrc ~/git/MythicalNewt/Mythical-Dots/zsh/.config/zsh
+cp ~/.config/zsh/neopush.sh ~/git/MythicalNewt/Mythical-Dots/zsh/.config/zsh
+
+git add -A
+git commit -m "Backup script."
+git push
