d2fa8eb30ba6753a2e17269ecf1d70b42719b8d6
MythicalNewt <89124240+MythicalNewt@users.noreply.github.com>
2023-08-31 18:42:03 +0530

    Cleaned up openrgb autostart script.

--- .config/autostart/openrgb.sh
+++ .config/autostart/openrgb.sh
@@ -1,5 +1,6 @@
 #!/bin/zsh
+
 # To add delay to the autostarting of openrgb
+/usr/bin/sleep 10
+export QT_QPA_PLATFORM=wayland;xcb
 /usr/bin/openrgb -p red --startminimized
-/bin/sleep 5
-/usr/bin/openrgb -p red 
