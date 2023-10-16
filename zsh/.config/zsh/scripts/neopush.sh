99126b9f06d2c67abce4c24ab3fc80be122a9f97
MythicalNewt <89124240+MythicalNewt@users.noreply.github.com>
2023-10-17 02:07:27 +0530

    Automatic push.

--- zsh/.config/zsh/scripts/neopush.sh
+++ zsh/.config/zsh/scripts/neopush.sh
@@ -9,7 +9,8 @@ git pull
 cp -r ~/.config/nvim/lua ~/git/MythicalNewt/Mythical-Dots/.config/nvim
 cp ~/.config/nvim/init.lua ~/git/MythicalNewt/Mythical-Dots/.config/nvim
 cp ~/.config/zsh/.zshrc ~/git/MythicalNewt/Mythical-Dots/zsh/.config/zsh
-cp ~/.config/zsh/neopush.sh ~/git/MythicalNewt/Mythical-Dots/zsh/.config/zsh
+cp ~/.config/zsh/scripts/neopush.sh ~/git/MythicalNewt/Mythical-Dots/zsh/.config/zsh/scripts
+cp ~/.config/zsh/scripts/grub.sh ~/git/MythicalNewt/Mythical-Dots/zsh/.config/zsh/scripts
 
 git commit -am "Automatic push."
 git push
