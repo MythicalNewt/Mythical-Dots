8425a5e6b91712cb79039c2f46b19b4178a27f49
MythicalNewt <89124240+MythicalNewt@users.noreply.github.com>
2023-12-19 13:13:53 +0530

    Fixed openrgb script for the last time hopefully.

--- .config/autostart/openrgb.sh
+++ .config/autostart/openrgb.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-# Running openrgb twice to fix it failing to set color profile to ram.
+# Running openrgb twice to fix it failing to set color profile.
 
+/usr/bin/openrgb -p red --startminimized &
+sleep 20
+killall openrgb
 /usr/bin/openrgb -p red --startminimized
-/usr/bin/sleep 10
-/usr/bin/killall openrgb
-/usr/bin/openrgb -p red --startminimized
+
